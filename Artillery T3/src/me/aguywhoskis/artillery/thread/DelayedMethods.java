6c41814ea21414473e2be94e18d97cef387b871f
AGuyWhoSkis <til@live.ca>
2013-12-22 23:54:13 -0500

    Added command annotation support

---
+++ Artillery T3/src/me/aguywhoskis/artillery/thread/DelayedMethods.java
@@ -0,0 +1,15 @@
+package me.aguywhoskis.artillery.thread;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class DelayedMethods {
+
+	BukkitRunnable myRunnable = new BukkitRunnable() {
+		
+		@Override
+		public void run() {
+			
+		}
+		
+	};
+}
